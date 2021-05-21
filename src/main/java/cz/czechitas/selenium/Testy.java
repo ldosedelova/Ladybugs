d1dc980305ca3be4ba91ae9c0fb9a920bab9bcf6
Lenka Dosedělová <dosedelova.l@seznam.cz>
2021-05-21 14:58:36 +0200

    Založení projektu Hackathon

---
+++ src/main/java/cz/czechitas/selenium/Testy.java
@@ -0,0 +1,41 @@
+package cz.czechitas.selenium;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Testy {
+
+    WebDriver prohlizec;
+
+    private static final String URL_APLIKACE = "";
+
+    @BeforeEach
+    public void setUp() {
+//      System.setProperty("webdriver.gecko.driver", System.getProperty("user.home") + "/Java-Training/Selenium/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "C:\\Java-Training\\Selenium\\geckodriver.exe");
+        prohlizec = new FirefoxDriver();
+        prohlizec.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void testJedna() {
+    }
+
+    @Test
+    public void testDva() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+        prohlizec.close();
+    }
+
+}
