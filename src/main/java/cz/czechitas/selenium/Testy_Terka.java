b8251b003523198bea9f42eda910a0446cf1b532
Tereza <reezy.kr@gmail.com>
2021-05-22 16:35:14 +0200

    pridani testovacich scenaru

--- src/main/java/cz/czechitas/selenium/Testy_Terka.java
+++ src/main/java/cz/czechitas/selenium/Testy_Terka.java
@@ -99,12 +99,132 @@ public class Testy_Terka {
         Assertions.assertNotNull(prihlaseni);
     }
 
+    @Test
+    public void mohuPrepnoutJazyk(){
+        prohlizec.navigate().to(URL_APLIKACE);
+        WebElement prepinacJazyku = prohlizec.findElement(By.id("languages-block-top"));
+        prepinacJazyku.click();
+        WebElement anglictina = prohlizec.findElement(By.xpath("//ul[@id = 'first-languages']/li/span"));
+        Assertions.assertEquals("English",anglictina.getText());
+        WebElement cestina = prohlizec.findElement(By.xpath("//ul[@id = 'first-languages']/li/a/span"));
+        Assertions.assertEquals("Čeština", cestina.getText());
+    }
+
+    @Test
+    public void mohuSePrepnoutDoCestiny(){
+        prohlizec.navigate().to(URL_APLIKACE);
+        WebElement prepinacJazyku = prohlizec.findElement(By.id("languages-block-top"));
+        prepinacJazyku.click();
+        WebElement cestinaLink = prohlizec.findElement(By.xpath("//ul[@id = 'first-languages']/li/a"));
+        cestinaLink.click();
+        String urlCestina =  prohlizec.getCurrentUrl();
+        Assertions.assertEquals("http://czechitas-datestovani-hackathon.cz/cs/", urlCestina);
+    }
+
+    @Test
+    public void mohuSeZeZahlaviDostatNaAutentizacniStranku(){
+        prohlizec.navigate().to(URL_APLIKACE);
+        WebElement linkPrihlaseni = prohlizec.findElement(By.xpath("//a[contains(@class, 'user_login')]"));
+        linkPrihlaseni.click();
+        String urlAutentizace = prohlizec.getCurrentUrl();
+        Assertions.assertEquals("http://czechitas-datestovani-hackathon.cz/en/login?back=my-account",urlAutentizace);
+    }
+
+
+ //   @Test
+//    public void MyPersonalInformation(){
+//        prohlizec.navigate().to(URL_APLIKACE);
+//        prihlaseniZHlavniStrany("qacustomer@gmail.com", "Asdf1234");
+//
+//        WebElement osobniInformace = prohlizec.findElement(By.xpath("//a[@title = 'information']"));
+//        osobniInformace.click();
+//
+//        WebElement title = prohlizec.findElement(By.xpath("//label[text(), Social title] "));
+//        title.isDisplayed();
+//        WebElement jmenoInput = prohlizec.findElement(By.id("firstname"));
+//        jmenoInput.isDisplayed();
+//        jmenoInput.isEnabled();
+//        WebElement prijmeniInput = prohlizec.findElement(By.id("lastname"));
+//        prijmeniInput.isDisplayed();
+//        prijmeniInput.isEnabled();
+//    }
+
+    @Test
+    public void mohuNajitPolozkuMenuHome(){
+        prohlizec.navigate().to(URL_APLIKACE);
+        WebElement menuTlacitko = prohlizec.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[3]/div/div/div[2]/button"));
+        menuTlacitko.click();
+        WebElement homeLink = prohlizec.findElement(By.linkText("Home"));
+        Assertions.assertNotNull(homeLink);
+        homeLink.click();
+        String urlHome = prohlizec.getCurrentUrl();
+        Assertions.assertEquals(URL_APLIKACE, urlHome);
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuInterior(){
+        najdiPolozkuMenu("Interior","#hotelInteriorBlock" );
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuAmenities(){
+        najdiPolozkuMenu("Amenities","#hotelAmenitiesBlock" );
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuRooms(){
+        najdiPolozkuMenu("Rooms","#hotelRoomsBlock" );
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuTestimonials(){
+        najdiPolozkuMenu("Testimonials","#hotelTestimonialBlock" );
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuLegalNotice(){
+        najdiPolozkuMenu("Legal Notice","#legalNotice" );
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuAboutUs(){
+        najdiPolozkuMenu("About us","#aboutUs" );
+    }
+
+    @Test
+    public void mohuNajitPolozkuMenuContatct(){
+        najdiPolozkuMenu("Contact","#contact" );
+    }
+
+    @Test
+    public void mohuZavritMenu(){
+        prohlizec.navigate().to(URL_APLIKACE);
+        WebElement menuTlacitko = prohlizec.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[3]/div/div/div[2]/button"));
+        menuTlacitko.click();
+        WebElement zaviraciTlacitko = prohlizec.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[3]/div/div/div[3]/div/div/div[1]/span"));
+        zaviraciTlacitko.click();
+        WebElement menu = prohlizec.findElement(By.id("menu_cont"));
+        String aktualniUmisteniClass = menu.getAttribute("class");
+        Assertions.assertEquals("menu_cont_right", aktualniUmisteniClass);
+    }
 
     @AfterEach
     public void tearDown() {
         prohlizec.close();
     }
 
+    public void najdiPolozkuMenu(String polozka, String mainPageAnchor){
+        prohlizec.navigate().to(URL_APLIKACE);
+        WebElement menuTlacitko = prohlizec.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[3]/div/div/div[2]/button"));
+        menuTlacitko.click();
+        WebElement interiorLink = prohlizec.findElement(By.linkText(polozka));
+        Assertions.assertNotNull(interiorLink, "Nelze nalezt link");
+        String anchor = interiorLink.getAttribute("href");
+        Assertions.assertEquals("http://czechitas-datestovani-hackathon.cz/" + mainPageAnchor, anchor);
+        interiorLink.click();
+        String urlHome = prohlizec.getCurrentUrl();
+        Assertions.assertEquals(URL_APLIKACE, urlHome, "Polozka neni na hlavni strane");
+    }
 
     public void prihlaseniZHlavniStrany(String email, String heslo) {
         WebElement linkPrihlaseni = prohlizec.findElement(By.xpath("//a[contains(@class, 'user_login')]"));
